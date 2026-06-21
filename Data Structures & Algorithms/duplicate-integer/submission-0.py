5a841b849b3d9f5697616bd864f920d96ea9d29b
Divyanshu Singhal <singhal.divyanshu@gmail.com>
2026-06-21 18:45:41 +0100

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        data_dict = defaultdict(int)
+        for num in nums:
+            if data_dict[num] == 0:
+                data_dict[num]+=1
+            elif data_dict[num] > 0:
+                return True
+        return False
+
+
+        
